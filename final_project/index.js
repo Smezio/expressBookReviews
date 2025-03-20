a745693254f420da80685b288f835c1916631979
Smezio <smerillriccardo@gmail.com>
2025-03-20 12:45:05 +0100

    Task 1

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const users = require('./router/auth_users.js').users
 
 const app = express();
 
@@ -12,6 +13,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if(req.headers.authorization) {
+        let token = req.headers.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if(err) {
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({message: 'User not authenticated'});
+            }
+        });
+    }
+    else {
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
